466d899da0196832784513bd83dcd257301224f6
zhoudaxia233 <yootaoo@gmail.com>
2019-05-16 19:02:12 +0200

    add unet with resnet encoder

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # PyTorch-Unet
 PyTorch1.0 Implementation of Unet
 
+- Vanilla Unet
+- Unet with ResNet encoder
+  - ResNet18
+  - ResNet34
+  - ResNet50
+  - ResNet101
+  - ResNet152
+- Unet with VGG encoder
+  - VGG13 with BatchNorm
+  - Very easy to be modified into other VGG architectures
+
 ## Vanilla U-Net architecture
 ![](img/u-net-architecture.png)
---
+++ resnet_unet.py
@@ -0,0 +1,150 @@
+import torch
+import torch.nn as nn
+from torchvision.models import resnet18, resnet34, resnet50, resnet101, resnet152
+
+
+def double_conv(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True)
+    )
+
+def up_conv(in_channels, out_channels):
+    return nn.ConvTranspose2d(
+        in_channels, out_channels, kernel_size=2, stride=2
+    )
+
+class SResUnet(nn.Module):
+    '''Shallow Unet with ResNet18 or ResNet34 encoder.
+    '''
+    def __init__(self, encoder, in_channels=3, out_channels=2):
+        super().__init__()
+        self.encoder = encoder
+        self.encoder_layers = list(self.encoder.children())
+
+        self.block1 = nn.Sequential(*self.encoder_layers[:3])
+        self.block2 = nn.Sequential(*self.encoder_layers[3:5])
+        self.block3 = self.encoder_layers[5]
+        self.block4 = self.encoder_layers[6]
+        self.block5 = self.encoder_layers[7]
+
+        self.up_conv6 = up_conv(512, 512)
+        self.conv6 = double_conv(512 + 256, 512)
+        self.up_conv7 = up_conv(512, 256)
+        self.conv7 = double_conv(256 + 128, 256)
+        self.up_conv8 = up_conv(256, 128)
+        self.conv8 = double_conv(128 + 64, 128)
+        self.up_conv9 = up_conv(128, 64)
+        self.conv9 = double_conv(64 + 64, 64)
+        self.up_conv10 = up_conv(64, 32)
+        self.conv10 = nn.Conv2d(32, out_channels, kernel_size=1)
+        self.sigmoid = nn.Sigmoid()
+
+        self._weights_init()
+
+    def _weights_init(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+    
+    def forward(self, x):
+        block1 = self.block1(x)
+        block2 = self.block2(block1)
+        block3 = self.block3(block2)
+        block4 = self.block4(block3)
+        block5 = self.block5(block4)
+
+        x = self.up_conv6(block5)
+        x = torch.cat([x, block4], dim=1)
+        x = self.conv6(x)
+
+        x = self.up_conv7(x)
+        x = torch.cat([x, block3], dim=1)
+        x = self.conv7(x)
+
+        x = self.up_conv8(x)
+        x = torch.cat([x, block2], dim=1)
+        x = self.conv8(x)
+
+        x = self.up_conv9(x)
+        x = torch.cat([x, block1], dim=1)
+        x = self.conv9(x)
+
+        x = self.up_conv10(x)
+        x = self.conv10(x)
+        x = self.sigmoid(x)
+
+        return x
+
+
+class DResUnet(nn.Module):
+    '''Deep Unet with ResNet50, ResNet101 or ResNet152 encoder.
+    '''
+    def __init__(self, encoder, in_channels=3, out_channels=2):
+        super().__init__()
+        self.encoder = encoder
+        self.encoder_layers = list(self.encoder.children())
+
+        self.block1 = nn.Sequential(*self.encoder_layers[:3])
+        self.block2 = nn.Sequential(*self.encoder_layers[3:5])
+        self.block3 = self.encoder_layers[5]
+        self.block4 = self.encoder_layers[6]
+        self.block5 = self.encoder_layers[7]
+
+        self.up_conv6 = up_conv(2048, 512)
+        self.conv6 = double_conv(512 + 1024, 512)
+        self.up_conv7 = up_conv(512, 256)
+        self.conv7 = double_conv(256 + 512, 256)
+        self.up_conv8 = up_conv(256, 128)
+        self.conv8 = double_conv(128 + 256, 128)
+        self.up_conv9 = up_conv(128, 64)
+        self.conv9 = double_conv(64 + 64, 64)
+        self.up_conv10 = up_conv(64, 32)
+        self.conv10 = nn.Conv2d(32, out_channels, kernel_size=1)
+        self.sigmoid = nn.Sigmoid()
+
+        self._weights_init()
+
+    def _weights_init(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+    
+    def forward(self, x):
+        block1 = self.block1(x)
+        block2 = self.block2(block1)
+        block3 = self.block3(block2)
+        block4 = self.block4(block3)
+        block5 = self.block5(block4)
+
+        x = self.up_conv6(block5)
+        x = torch.cat([x, block4], dim=1)
+        x = self.conv6(x)
+
+        x = self.up_conv7(x)
+        x = torch.cat([x, block3], dim=1)
+        x = self.conv7(x)
+
+        x = self.up_conv8(x)
+        x = torch.cat([x, block2], dim=1)
+        x = self.conv8(x)
+
+        x = self.up_conv9(x)
+        x = torch.cat([x, block1], dim=1)
+        x = self.conv9(x)
+
+        x = self.up_conv10(x)
+        x = self.conv10(x)
+        x = self.sigmoid(x)
+
+        return x
